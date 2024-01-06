4e3864b48eb7a632ede447303daac702f2b9fca9
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-06 23:25:47 +0200

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""pascal's triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    lists = [[1]]
+    for row in range(1, n):
+        tmp = [1]
+        for i in range(len(lists[row - 1]) - 1):
+            tmp.append(lists[row - 1][i] + lists[row - 1][i + 1])
+        tmp.append(1)
+        lists.append(tmp)
+    return lists
