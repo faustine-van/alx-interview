c2dc53dfc98392af43584a583b03abea27c35aa6
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-12 23:16:10 +0200

    check if all boxes can be unlocked

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""check Unlock box"""
+
+
+def canUnlockAll(boxes):
+    """Return True if all boxes
+       can be opened,
+       else return False
+    """
+
+    unlockedOnes = [0]
+    for id, box in enumerate(boxes):
+        if not box:
+            continue
+        for val in box:
+            if val not in unlockedOnes and val != id:
+                unlockedOnes.append(val)
+    if len(boxes) != len(unlockedOnes):
+        return False
+    return True
