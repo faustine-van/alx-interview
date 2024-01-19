7d0ca5abe93b954e3132f556f18f52ca4c7e639b
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-19 12:35:45 +0200

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,13 +12,16 @@ def minOperations(n):
     if n <= 0:
         return 0
     prime_factors = []
+    # init 2 as smallest prime number
     ops = 2
     while ops * ops <= n:
         if n % ops == 0:
             prime_factors.append(ops)
+            # update n ex: 6 (12 // 2).
             n //= ops
         else:
             ops = ops + 1
+    # in case n is 1, so no action is needed.
     if n > 1:
         prime_factors.append(n)
     return sum(prime_factors)
