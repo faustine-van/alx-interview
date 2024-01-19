b7415a84d700f08a4104906049f8b5a124e002a0
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-19 12:14:33 +0200

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,15 +9,16 @@ def minOperations(n):
        n: give number
        return min operations
     """
-    text = "H"
-    result = [text]
-    for _ in range(2):
-        # count = 1
-        #  if len(result[-1]) <= n:
-        text += text
-        result.append(text)
-        if len(result[-1]) <= n:
-            text = result[-1]
-            result.append(text)
-        # count = count + 1
-    return result
+    if n <= 0:
+        return 0
+    prime_factors = []
+    ops = 2
+    while ops * ops <= n:
+        if n % ops == 0:
+            prime_factors.append(ops)
+            n //= ops
+        else:
+            ops = ops + 1
+    if n > 1:
+        prime_factors.append(n)
+    return sum(prime_factors)
