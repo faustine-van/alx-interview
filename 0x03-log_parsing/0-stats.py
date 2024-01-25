eaf5924e43c3771418b59cd44255e595faf5e4b2
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-25 23:57:37 +0200

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,31 +12,32 @@ def print_statistics(total_size, status_code_counts):
         print(f"{code}: {status_code_counts[code]}")
 
 
-line_number = 0
-codes = [200, 301, 400, 401, 403, 404, 405, 500]
-sumAll = 0
-times = []
-n_of_counts = {}
-try:
-    for line in sys.stdin:
-        args = line.split()
-        try:
-            sumAll += int(args[-1])
-            times.append(int(args[-2]))
-        except (ValueError, IndexError):
-            continue
-        line_number += 1
-        if line_number % 10 == 0:
-            for i in sorted(times):
-                if i is None or not isinstance(i, int):
-                    continue
-                if i in n_of_counts and i in codes:
-                    n_of_counts[i] += 1
-                else:
-                    n_of_counts[i] = 1
-            print_statistics(sumAll, n_of_counts)
-            times.clear()
-except KeyboardInterrupt:
+if __name__ == "__main__":
+    line_number = 0
+    codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    sumAll = 0
+    times = []
+    n_of_counts = {}
+    try:
+        for line in sys.stdin:
+            args = line.split()
+            try:
+                sumAll += int(args[-1])
+                times.append(int(args[-2]))
+            except (ValueError, IndexError):
+                continue
+            line_number += 1
+            if line_number % 10 == 0:
+                for i in sorted(times):
+                    if i is None or not isinstance(i, int):
+                        continue
+                    if i in n_of_counts and i in codes:
+                        n_of_counts[i] += 1
+                    else:
+                        n_of_counts[i] = 1
+                print_statistics(sumAll, n_of_counts)
+                times.clear()
+    except KeyboardInterrupt:
+        print_statistics(sumAll, n_of_counts)
+        sys.exit(0)
     print_statistics(sumAll, n_of_counts)
-    sys.exit(0)
-print_statistics(sumAll, n_of_counts)
