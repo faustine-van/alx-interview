58b93aa75516e88c662ddcb37c18523762bdda58
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-25 23:09:10 +0200

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,32 +12,29 @@ def print_statistics(total_size, status_code_counts):
         print(f"{code}: {status_code_counts[code]}")
 
 
-if __name__ == "__main__":
+line_number = 0
+codes = [200, 301, 400, 401, 403, 404, 405, 500]
+sumAll = 0
+times = []
+n_of_counts = {}
+try:
+    for line in sys.stdin:
+        args = line.split()
+        if len(args) == 9:
+            sumAll += int(args[-1])
+            times.append(int(args[-2]))
+            line_number += 1
 
-    line_number = 0
-
-    codes = [200, 301, 400, 401, 403, 404, 405, 500]
-    sumAll = 0
-    times = []
-    n_of_counts = {}
-    try:
-        for line in sys.stdin:
-            args = line.split()
-            if len(args) == 9:
-                sumAll += int(args[-1])
-                times.append(int(args[-2]))
-                line_number += 1
-
-                if line_number % 10 == 0:
-                    for i in sorted(times):
-                        if i is None or not isinstance(i, int):
-                            continue
-                        if i in n_of_counts and i in codes:
-                            n_of_counts[i] += 1
-                        else:
-                            n_of_counts[i] = 1
-                    print_statistics(sumAll, n_of_counts)
-                    times.clear()
-    except KeyboardInterrupt:
-        print_statistics(sumAll, n_of_counts)
-        sys.exit(0)
+            if line_number % 10 == 0:
+                for i in sorted(times):
+                    if i is None or not isinstance(i, int):
+                        continue
+                    if i in n_of_counts and i in codes:
+                        n_of_counts[i] += 1
+                    else:
+                        n_of_counts[i] = 1
+                print_statistics(sumAll, n_of_counts)
+                times.clear()
+except KeyboardInterrupt:
+    print_statistics(sumAll, n_of_counts)
+    sys.exit(0)
