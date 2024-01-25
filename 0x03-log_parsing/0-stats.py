88736b63935b392f97ede7153cec7f3664920452
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-25 22:58:39 +0200

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,51 +2,42 @@
 """script that reads stdin line by line
    and computes metrics
 """
-import fileinput
-import re
 import sys
+
+
+def print_statistics(total_size, status_code_counts):
+    """print"""
+    print(f"File size: {total_size}")
+    for code in sorted(status_code_counts):
+        print(f"{code}: {status_code_counts[code]}")
 
 
 if __name__ == "__main__":
 
     line_number = 0
-    ip = r'(\d+\.\d+\.\d+\.\d+)'
-    date = r'\[(\d+-\d+-\d+ \d+:\d+:\d+\.\d+)\]'
-    status = r'"GET \/projects\/260 HTTP\/1\.1"'
-    match = f'{ip} - {date} {status} (\\d+) (\\d+)'
 
     codes = [200, 301, 400, 401, 403, 404, 405, 500]
-    sumAll = []
+    sumAll = 0
     times = []
     n_of_counts = {}
     try:
         for line in sys.stdin:
-            if not re.search(match, line):
-                continue
-            line_number += 1
             args = line.split()
-            sumAll.append(int(args[-1]))
-            times.append(int(args[-2]))
+            if len(args) == 9:
+                sumAll += int(args[-1])
+                times.append(int(args[-2]))
+                line_number += 1
 
-            if line_number % 10 == 0:
-                # after 10 line do this
-                # print(times)
-                for i in sorted(times):
-                    if i is None or not isinstance(i, int):
-                        continue
-                    if i in n_of_counts and i in codes:
-                        n_of_counts[i] += 1
-                    else:
-                        n_of_counts[i] = 1
-                print(f'File size: {sum(sumAll)}')
-                for num, val in n_of_counts.items():
-                    for code in sorted(codes):
-                        if code == num:
-                            print(f'{num}: {val}')
-                times.clear()
+                if line_number % 10 == 0:
+                    for i in sorted(times):
+                        if i is None or not isinstance(i, int):
+                            continue
+                        if i in n_of_counts and i in codes:
+                            n_of_counts[i] += 1
+                        else:
+                            n_of_counts[i] = 1
+                    print_statistics(sumAll, n_of_counts)
+                    times.clear()
     except KeyboardInterrupt:
-        for num, val in n_of_counts.items():
-            for code in sorted(codes):
-                if code == num:
-                    print(f'{num}: {val}')
+        print_statistics(sumAll, n_of_counts)
         sys.exit(0)
