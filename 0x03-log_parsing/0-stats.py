8201e4a97ea5d0d9d76c710e7f432e30d78a4573
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-26 10:05:44 +0200

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,43 +7,38 @@ import sys
 
 def print_statistics(total_size, status_code_counts):
     """print"""
-    if total_size > 0:
-        print(f"File size: {total_size}")
+    print(f"File size: {total_size}")
     for code in sorted(status_code_counts):
-        if status_code_counts[code] > 0:
-            print(f"{code}: {status_code_counts[code]}")
+        print(f"{code}: {status_code_counts[code]}")
 
 
 if __name__ == "__main__":
     line_number = 0
     codes = [200, 301, 400, 401, 403, 404, 405, 500]
     sumAll = 0
-    times = []
-    n_of_counts = {}
+    stats = {k: 0 for k in codes}
+
     try:
         for line in sys.stdin:
+            line_number += 1
             args = line.split()
             try:
                 sumAll += int(args[-1])
-                times.append(int(args[-2]))
+            except BaseException:
+                pass
+            try:
+                status_code = int(args[-2])
+                if status_code in stats:
+                    stats[status_code] += 1
+                else:
+                    stats[status_code] = 1
             except BaseException:
                 pass
             # print(times)
-            line_number += 1
             if line_number % 10 == 0:
-                try:
-                    for i in sorted(times):
-                        if i is None or not isinstance(i, int):
-                            continue
-                        if i in n_of_counts and i in codes:
-                            n_of_counts[i] += 1
-                        else:
-                            n_of_counts[i] = 1
-                except BaseException:
-                    pass
-                print_statistics(sumAll, n_of_counts)
+                print_statistics(sumAll, stats)
                 times = []
+        print_statistics(sumAll, stats)
     except KeyboardInterrupt:
-        print_statistics(sumAll, n_of_counts)
+        print_statistics(sumAll, stats)
         sys.exit(0)
-    print_statistics(sumAll, n_of_counts)
